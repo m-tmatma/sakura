6c82d2b0f2f9ed78bcf0e5fc893c37ffdd8328eb
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-09-15 21:54:23 +0900

    MingW のパスを変更

--- build-mingw-appveyor.bat
+++ build-mingw-appveyor.bat
@@ -4,7 +4,7 @@ set CONFIFURATION=%2
 if "%PLATFORM%" == "Win32" (
 	set MINGW_PATH=C:\MinGW\bin
 ) else if "%PLATFORM%" == "x64" (
-	set MINGW_PATH=C:\mingw-w64\x86_64-6.3.0-posix-seh-rt_v5-rev1
+	set MINGW_PATH=C:\mingw-w64\x86_64-7.2.0-posix-seh-rt_v5-rev1
 ) else (
 	@echo unsupported platform
 	exit /b 1
