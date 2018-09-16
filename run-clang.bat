cc0b7f7b16b3f10c0e768ef7f33f31c8aa63c508
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-09-17 06:35:58 +0900

    [ci skip] sakura_core 以下の cpp ファイルを非再帰的に処理する

--- run-clang.bat
+++ run-clang.bat
@@ -1,2 +1,5 @@
 set CLANG_FORMAT=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VC\vcpackages\clang-format.exe
-"%CLANG_FORMAT%" -style=llvm -dump-config > .clang-format
+for %%i in (sakura_core\*.cpp) do (
+	git checkout -f                 %%i
+	"%CLANG_FORMAT%" -style=file -i %%i
+)
