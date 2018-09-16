36348e5a9b9369fd013f9fe7cbaf84405b3fcb01
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-09-17 08:49:48 +0900

    [ci skip] 再帰的に処理対象を検索する

--- run-clang.bat
+++ run-clang.bat
@@ -1,9 +1,9 @@
 set CLANG_FORMAT=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VC\vcpackages\clang-format.exe
-for %%i in (sakura_core\*.cpp) do (
+for /r %%i in (*.cpp) do (
 	git checkout -f                 %%i
 	"%CLANG_FORMAT%" -style=file -i %%i
 )
-for %%i in (sakura_core\*.h) do (
+for /r %%i in (*.h) do (
 	git checkout -f                 %%i
 	"%CLANG_FORMAT%" -style=file -i %%i
 )
