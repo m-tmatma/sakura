3ba3bb9e525a223b4f284194cb23d0c362fa72cf
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-09-17 09:02:35 +0900

    [ci skip] 関数に分けて処理対象を絞る

--- run-clang.bat
+++ run-clang.bat
@@ -1,9 +1,23 @@
-set CLANG_FORMAT=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VC\vcpackages\clang-format.exe
-for /r %%i in (*.cpp) do (
-	git checkout -f                 %%i
-	"%CLANG_FORMAT%" -style=file -i %%i
-)
-for /r %%i in (*.h) do (
-	git checkout -f                 %%i
-	"%CLANG_FORMAT%" -style=file -i %%i
-)
+@echo off
+call :run-format-recursive              sakura_core
+exit /b 0
+
+:run-format-recursive
+	pushd %1
+	for /r %%i in (*.cpp) do (
+		call :run-format                %%i
+	)
+	for /r %%i in (*.h) do (
+		call :run-format                %%i
+	)
+	popd
+	exit /b 0
+
+:run-format
+	pushd %~dp0
+	set CLANG_FORMAT=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VC\vcpackages\clang-format.exe
+	git checkout -f                %1
+	@echo "%CLANG_FORMAT%" -style=file -i %1
+	      "%CLANG_FORMAT%" -style=file -i %1
+	popd
+	exit /b 0
