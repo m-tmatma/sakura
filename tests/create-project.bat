80956bf925aadb11b0f36a66edb66a758f374acd
Masaru Tsuchiyama <m.tmatma@gmail.com>
2019-01-27 07:12:45 +0900

    Ninja を使う

--- tests/create-project.bat
+++ tests/create-project.bat
@@ -55,7 +55,7 @@ exit /b
 
 :setenv_Win32
 :setenv_x64
-	set CMAKE_GEN_OPT=-G "Visual Studio 15 2017" -A "%~1" -D BUILD_GTEST=ON
+	set CMAKE_GEN_OPT=-G Ninja -A "%~1" -D BUILD_GTEST=ON
 exit /b
 
 :setenv_MinGW
