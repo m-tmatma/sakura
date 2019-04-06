a8fde3bf060bcaf2f649ef2c161af6894d32cc29
Masaru Tsuchiyama <m.tmatma@gmail.com>
2019-04-07 08:32:46 +0900

    [ci skip] 昔作ったデータを保存 (詳細を覚えてないので保存)

---
+++ create-clang-format.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VC\vcpackages\clang-format.exe"  -dump-config -style=llvm > clang-format.old.txt
+"C:\Program Files\LLVM\bin\clang-format.exe"  -dump-config -style=llvm > clang-format.new.txt
