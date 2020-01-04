99afeb9c3db34b6b6ee4424f343c0656713600b2
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-01-04 09:39:14 +0900

    IDC_BUTTON_TOPFOLDER -> IDC_BUTTON_GITROOT_FOLDER

--- sakura_core/dlg/CDlgGrep.cpp
+++ sakura_core/dlg/CDlgGrep.cpp
@@ -467,7 +467,7 @@ BOOL CDlgGrep::OnBnClicked( int wID )
 		}
 		return TRUE;
 
-	case IDC_BUTTON_TOPFOLDER:
+	case IDC_BUTTON_GITROOT_FOLDER:
 		{
 			HWND hwnd = GetItemHwnd( IDC_COMBO_FOLDER );
 			const int nMaxPath = MAX_GREP_PATH;
