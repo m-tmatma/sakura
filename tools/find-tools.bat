d2fb8ee946935d1fcc3ff1818c322fe937fb1951
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-01-13 19:57:54 +0900

    順番変更

--- tools/find-tools.bat
+++ tools/find-tools.bat
@@ -41,10 +41,10 @@ echo ^|- CMD_CPPCHECK=%CMD_CPPCHECK%
 echo ^|- CMD_DOXYGEN=%CMD_DOXYGEN%
 echo ^|- CMD_VSWHERE=%CMD_VSWHERE%
 echo ^|- CMD_MSBUILD=%CMD_MSBUILD%
-echo ^|- CMD_CMAKE=%CMD_CMAKE%
 echo ^|- NUM_VSVERSION=%NUM_VSVERSION%
 echo ^|- PARAM_VSVERSION=%PARAM_VSVERSION%
 echo ^|- CMAKE_G_PARAM=%CMAKE_G_PARAM%
+echo ^|- CMD_CMAKE=%CMD_CMAKE%
 endlocal ^
     && set "CMD_GIT=%CMD_GIT%"                  ^
     && set "CMD_7Z=%CMD_7Z%"                    ^
