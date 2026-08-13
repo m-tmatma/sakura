67ece6303ba6c366d6ff6c4b42f4b98004becb18
Masaru Tsuchiyama <m.tmatma@gmail.com>
2026-08-13 16:29:08 +0900

    Pythonマクロ: DLL検索パスの設定をシンボル解決完了まで維持する
    python3.dllの各APIは実体DLL(python3xx.dll)へのフォワーダーであり、
    その解決はLoadLibrary時ではなくGetProcAddress呼び出し時に遅延して
    行われる。そのためLoadLibraryExedir()の直後にSetDllDirectoryW(L"")で
    検索パスを元に戻していると、python3xx.dllがPATHに含まれない環境では
    最初のGetProcAddress("Py_Initialize")から解決に失敗していた。
    
    SetDllDirectoryW(L"")によるリセットをシンボル解決ループの後(成功/
    失敗いずれの場合も)に移動し、フォワーダー解決が完了するまで検索パスを
    維持するようにする。

--- sakura_core/macro/CPythonMacroManager.cpp
+++ sakura_core/macro/CPythonMacroManager.cpp
@@ -979,14 +979,17 @@ bool CPythonMacroManager::ExecKeyMacro(CEditView *EditView, int flags [[maybe_un
 			}
 		}
 		if (bUseDllDirectory) {
-			// python3.dll が依存するDLL(python3xx.dll等)を指定フォルダーから検索できるようにする
+			// python3.dllが依存するDLL(python3xx.dll等)を指定フォルダーから検索できるようにする。
+			// python3.dllのAPIは実体(python3xx.dll)へのフォワーダーであり、python3xx.dllの解決は
+			// LoadLibrary時ではなくGetProcAddress時に遅延して行われるため、シンボル解決が終わるまで
+			// 検索パスを維持する必要がある。
 			::SetDllDirectoryW(dir);
 		}
 		s_hModule = LoadLibraryExedir(path.c_str());
-		if (bUseDllDirectory) {
-			::SetDllDirectoryW(L"");
-		}
 		if (!s_hModule) {
+			if (bUseDllDirectory) {
+				::SetDllDirectoryW(L"");
+			}
 			WCHAR* pMsg;
 			::FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER |
 				FORMAT_MESSAGE_IGNORE_INSERTS |
@@ -1009,12 +1012,18 @@ bool CPythonMacroManager::ExecKeyMacro(CEditView *EditView, int flags [[maybe_un
 			auto sym = ::GetProcAddress(s_hModule, s.name);
 			if (!sym) {
 				// 使用中のpython3.dllがこの機能に必要なシンボルをエクスポートしていない
+				if (bUseDllDirectory) {
+					::SetDllDirectoryW(L"");
+				}
 				ErrorMessage(nullptr, L"python3.dll: GetProcAddress(\"%hs\") failed.", s.name);
 				::FreeLibrary(s_hModule);
 				s_hModule = nullptr;
 				return false;
 			}
 			*(void**)s.ptr = (void*)sym;
+		}
+		if (bUseDllDirectory) {
+			::SetDllDirectoryW(L"");
 		}
 	}
 
